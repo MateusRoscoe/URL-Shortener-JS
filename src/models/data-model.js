1ba27e07dc719939cbead9cf1ff9b334926f810e
Mateus Roscoe <mateusroscoe2@gmail.com>
2024-05-19 16:51:16 -0300

    chore: cleanup

--- src/models/data-model.js
+++ src/models/data-model.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 const client = new MongoClient(process.env.MONGODB_URI)
 const collection = client
-    .db(process.env.MONGO_DB_NAME)
+    .db(process.env.MONGO_DATABASE)
     .collection(process.env.MONGO_COLLECTION_NAME)
 
 /**
