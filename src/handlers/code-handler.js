3db687d50a62b183bada00f451b5c462e46ddb57
Mateus Roscoe <mateusroscoe2@gmail.com>
2024-05-19 17:55:08 -0300

    feat: handling code not found

--- src/handlers/code-handler.js
+++ src/handlers/code-handler.js
@@ -1,12 +1,27 @@
 const CodeService = require('../services/code-service')
+const { Request, Response } = require('express')
 
 class CodeHandler {
+    /**
+     * Handles GET requests to /code.
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     */
     static async get(req, res) {
         const code = req.query.code
         const data = await CodeService.getByCode(code)
+        if (!data) {
+            res.sendStatus(404)
+            return
+        }
         res.send(data)
     }
 
+    /**
+     * Handles POST requests to /code.
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     */
     static async post(req, res) {
         const data = req.body.data
         const code = await CodeService.generateCode(data)
